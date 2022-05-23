aa3850619d22096a19a3972f3dc8d6fa31ae7fe4
Jagadeesh-palanisamy <88223245+Jagadeesh-palanisamy@users.noreply.github.com>
2022-05-23 18:32:53 +0530

    Update statistics.js

--- app/statistics.js
+++ app/statistics.js
@@ -1,11 +1,21 @@
 module.exports=function getStatistics(numbers){
  
+function getStatistics(numbers){
+ 
 let total=0;
+if(numbers.length>0){
 let average = numbers.reduce(function (previousValue, currentValue) {
     return previousValue + currentValue;
 });
 
 let result={min:Math.min(...numbers),max:Math.max(...numbers),average:average}
+console.log(result)
 return result;
+}else{
+ let result={min:'NAN',max:'NAN',average:'NAN'}
+console.log(result)
+return result; 
+}
+}
 }
 
